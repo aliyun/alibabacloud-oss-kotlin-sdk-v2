be9ee4c555c22fad428106ec15d85082a9097c32
ws <wb-ws775196@alibaba-inc.com>
2026-03-03 10:52:30 +0800

    add api BucketLogging

---
+++ oss-sdk-extension/src/commonMain/kotlin/com/aliyun/kotlin/sdk/service/oss2/extension/models/LoggingEnabled.kt
@@ -0,0 +1,33 @@
+package com.aliyun.kotlin.sdk.service.oss2.extension.models
+
+import com.aliyun.kotlin.sdk.service.oss2.serialization.xml.XmlElement
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+/**
+ * The container that stores the information about access log collection.
+ */
+@Serializable
+@SerialName("LoggingEnabled")
+public data class LoggingEnabled(
+    /**
+     * The bucket that stores access logs.
+     */
+    @XmlElement("TargetBucket") public var targetBucket: String? = null,
+
+    /**
+     * The prefix of the log objects. This parameter can be left empty.
+     */
+    @XmlElement("TargetPrefix") public var targetPrefix: String? = null,
+
+    /**
+     * The logging role.
+     */
+    @XmlElement("LoggingRole") public var loggingRole: String? = null
+) {
+    public companion object {
+        public operator fun invoke(builder: LoggingEnabled.() -> Unit): LoggingEnabled =
+            LoggingEnabled().apply(builder)
+    }
+}
---
+++ oss-sdk-extension/src/commonMain/kotlin/com/aliyun/kotlin/sdk/service/oss2/extension/models/ParamSet.kt
@@ -0,0 +1,23 @@
+package com.aliyun.kotlin.sdk.service.oss2.extension.models
+
+import com.aliyun.kotlin.sdk.service.oss2.serialization.xml.XmlElement
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+/**
+ * The container that stores the configurations of custom URL parameters.
+ */
+@Serializable
+@SerialName("ParamSet")
+public data class ParamSet(
+    /**
+     * The list of the custom URL parameters.
+     */
+    @XmlElement("parameter") public var parameters: List<String>? = null
+) {
+    public companion object {
+        public operator fun invoke(builder: ParamSet.() -> Unit): ParamSet =
+            ParamSet().apply(builder)
+    }
+}
