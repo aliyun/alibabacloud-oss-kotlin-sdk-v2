cd5ac89181be72a20945ebaba249c7ac36bdd05b
ws <wb-ws775196@alibaba-inc.com>
2026-03-03 10:52:30 +0800

    add api BucketLogging

---
+++ oss-sdk-extension/src/commonMain/kotlin/com/aliyun/kotlin/sdk/service/oss2/extension/models/GetBucketLoggingRequest.kt
@@ -0,0 +1,40 @@
+package com.aliyun.kotlin.sdk.service.oss2.extension.models
+
+/**
+ * The request for the GetBucketLogging operation.
+ */
+public class GetBucketLoggingRequest(builder: Builder): RequestModel(builder) {
+    
+    /**
+     * The name of the bucket.
+     */
+    public val bucket: String? = builder.bucket
+    
+
+    public inline fun copy(block: Builder.() -> Unit = {}): GetBucketLoggingRequest = Builder(this).apply(block).build()
+
+    public companion object {
+        public operator fun invoke(builder: Builder.() -> Unit): GetBucketLoggingRequest =
+            Builder().apply(builder).build()
+    }
+
+    public class Builder(): RequestModel.Builder() {
+    
+        /**
+        * The name of the bucket.
+        */
+        public var bucket: String? = null
+    
+        
+        public fun build(): GetBucketLoggingRequest {
+            return GetBucketLoggingRequest(this)
+        }
+
+        public constructor(from: GetBucketLoggingRequest): this() {
+            this.headers.putAll(from.headers)
+            this.parameters.putAll(from.parameters) 
+            this.bucket = from.bucket 
+        }             
+    }
+
+}
---
+++ oss-sdk-extension/src/commonMain/kotlin/com/aliyun/kotlin/sdk/service/oss2/extension/models/PutBucketLoggingRequest.kt
@@ -0,0 +1,51 @@
+package com.aliyun.kotlin.sdk.service.oss2.extension.models
+
+/**
+ * The request for the PutBucketLogging operation.
+ */
+public class PutBucketLoggingRequest(builder: Builder): RequestModel(builder) {
+    
+    /**
+     * The name of the bucket.
+     */
+    public val bucket: String? = builder.bucket
+    
+    /**
+     * The request body schema.
+     */
+    public var bucketLoggingStatus: BucketLoggingStatus? = builder.bucketLoggingStatus
+    
+
+    public inline fun copy(block: Builder.() -> Unit = {}): PutBucketLoggingRequest = Builder(this).apply(block).build()
+
+    public companion object {
+        public operator fun invoke(builder: Builder.() -> Unit): PutBucketLoggingRequest =
+            Builder().apply(builder).build()
+    }
+
+    public class Builder(): RequestModel.Builder() {
+    
+        /**
+        * The name of the bucket.
+        */
+        public var bucket: String? = null
+    
+        /**
+        * The request body schema.
+        */
+        public var bucketLoggingStatus: BucketLoggingStatus? = null
+    
+        
+        public fun build(): PutBucketLoggingRequest {
+            return PutBucketLoggingRequest(this)
+        }
+
+        public constructor(from: PutBucketLoggingRequest): this() {
+            this.headers.putAll(from.headers)
+            this.parameters.putAll(from.parameters) 
+            this.bucket = from.bucket 
+            this.bucketLoggingStatus = from.bucketLoggingStatus 
+        }             
+    }
+
+}
