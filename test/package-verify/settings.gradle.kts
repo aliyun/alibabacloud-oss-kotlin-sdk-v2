f773b46e4f64e93a0510b2b21b065650f3a08c9b
ws <wb-ws775196@alibaba-inc.com>
2026-07-24 15:48:46 +0800

    add package verify

---
+++ test/package-verify/settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "package-verify"
