c0174f4b909e46c41270d6e41adc321e02935931
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2026-08-12 18:19:06 +0800

    add agentic sample code

--- sample/cli/src/commonMain/kotlin/sample/app/main.kt
+++ sample/cli/src/commonMain/kotlin/sample/app/main.kt
@@ -46,7 +46,15 @@ fun main(args: Array<String>) {
         PutObjectTagging(),
         GetObjectTagging(),
         DeleteObjectTagging(),
-        Presign()
+        Presign(),
+        CreateAgenticBucket(),
+        DeleteAgenticBucket(),
+        GetAgenticBucket(),
+        ListAgenticBuckets(),
+        PutAgenticBucketStatus(),
+        ListBucketSpaces(),
+        CreateBucketSpace(),
+        BucketSpace()
     )
     parser.parse(args)
 }
